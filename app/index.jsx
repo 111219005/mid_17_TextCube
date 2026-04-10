f61cc9832234c606bc53907de6a7f15f21164e31
mr77tobey <s111219005@stu.ntue.edu.tw>
2026-04-11 05:24:45 +0800

    新增 app name, icon, splash

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,42 @@
+import * as SplashScreen from 'expo-splash-screen';
 import { StyleSheet, View, StatusBar, Text } from "react-native";
+import { useState, useEffect } from "react";
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import TopBar from "../components/TopBar.jsx";
 import ImageUploadBox from "../components/ImageUploadBox.jsx";
 import BackgroundImage from "../components/BackgroundImage.jsx";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Page() {
 
     const insets = useSafeAreaInsets();
+
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await new Promise(resolve => setTimeout(resolve, 2000));
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                setAppIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    useEffect(() => {
+        if (appIsReady) {
+            SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
+
+    if (!appIsReady) {
+        return null;
+    }
 
     return (
         <SafeAreaProvider>
@@ -14,7 +44,7 @@ export default function Page() {
                 <StatusBar barStyle="dark-content" backgroundColor="transparent" />
                 <View style={[styles.container, { paddingTop: insets.top }]}>
                     <View style={styles.main}>
-                        <TopBar />             
+                        <TopBar />
                         <ImageUploadBox />
                     </View>
                 </View>
